023d57ada3bb08f7cec1cfaf5bba77c3358c40bd
HavisNih <havislutfi4@gmail.com>
2026-09-03 09:51:33 +0700

    praktikum pekan1_2611531016

---
+++ pekan1/program1_1016.py
@@ -0,0 +1,14 @@
+# Program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum algro
+"""
+print("hello world")
+print( )
+print("program ini menghasilkan")
+print("empat baris output")
+print("/ \\ // \\\\ /// \\\\\ ")
+print("program ini" + "dibuat oleh a\n" 
++ "Havis Lutfi 2611531016")
+      
